8fed34fe7b0dfedb195c18b6efad7eb4c6b3690f
Aswin Murali <aswinmurali.co@gmail.com>
2021-06-05 15:09:32 +0530

    .

--- lib/routes/batch_edits/batch_edits.dart
+++ lib/routes/batch_edits/batch_edits.dart
@@ -68,6 +68,37 @@ class DesktopManager {
     }
   }
 
+  static Future<void> batchExt(
+      String src, String des, BuildContext context) async {
+    final files = Glob(src);
+
+    for (FileSystemEntity entity in files.listSync()) {
+      final filename = basename(entity.path);
+      final 
+      print('$des/${filename.split('.').first}');
+      try {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Executing...'),
+          duration: const Duration(seconds: 1),
+        ));
+        try {
+          await File(entity.path).rename('$des/$filename');
+        } catch (e) {
+          await Process.run('mv', ['-f', src, des]);
+        }
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Finished'),
+          duration: const Duration(seconds: 1),
+        ));
+      } on FileSystemException {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Failed'),
+          duration: const Duration(seconds: 1),
+        ));
+      }
+    }
+  }
+
   static Future<void> batchDelete(String src, BuildContext context) async {
     final files = Glob(src);
 
@@ -91,4 +122,6 @@ class DesktopManager {
       ));
     }
   }
+
+  
 }
